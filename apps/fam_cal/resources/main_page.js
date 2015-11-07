1bb414b6a8739c18c9c00097cedf7b708e494c43
WendyGoller <wendy.goller@uniconnect.com>
2015-11-07 12:03:30 -0700

    first commit

---
+++ apps/fam_cal/resources/main_page.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   FamCal - mainPage
+// Copyright: @2015 My Company, Inc.
+// ==========================================================================
+/*globals FamCal */
+
+// This page describes the main user interface for your application.
+FamCal.mainPage = SC.Page.design({
+
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page
+  // load.
+  mainPane: SC.MainPane.design({
+    childViews: ['labelView'],
+
+    labelView: SC.LabelView.design({
+      classNames: ['welcome-label'],
+      layout: { centerX: 0, centerY: 0, width: 300, height: 24 },
+      tagName: "h1",
+      value: "Welcome to SproutCore!"
+    })
+  })
+
+});
